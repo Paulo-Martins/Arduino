ef30384ce43a46afdd9c4fd2a257b756cc259af1
pmartins <braga82@gmail.com>
2016-04-24 02:05:57 +0100

    primeiro commit

--- Sketch1/Sketch1.ino
+++ Sketch1/Sketch1.ino
@@ -6,10 +6,11 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+	Serial.begin(9600);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+	pinMode(10, OUTPUT);
+		pinMode(9, 1);
 }
